daa184b98f94837b3dfc98a2a91a2a55df939717
Marco Caselli <casellimarco7@gmail.com>
2021-11-13 19:57:28 +0000

    Rename + Refactor + add test

--- dast/lcs.py
+++ dast/lcs.py
@@ -4,43 +4,43 @@ Longest Common Subsequence problem
 from collections import defaultdict
 from typing import Any, Dict, Iterable, List, Tuple
 
-def get_lcs(x: Iterable[Any], y: Iterable[Any]):
+def get_lcs(xs: Iterable[Any], ys: Iterable[Any]):
     """
     Longest common subsequence between two finite iterables via dynamic programming.
 
     >>> get_lcs("abcde", "abfgdfe")
     (['a', 'b', 'd', 'e'], [True, True, False, True, True], [True, True, False, False, True, False, True])
     """
-    all_x = list(x)
-    all_y = list(y)
-    num_x = len(all_x)
-    num_y = len(all_y)
-
     # sub_problems[i, j] = LCS between all_x[:i] and all_y[:j]
     sub_problems: Dict[Tuple[int, int], List] = defaultdict(list)
 
-    for i in range(num_x):
-        for j in range(num_y):
-            if all_x[i] == all_y[j]:
-                sub_problems[i + 1, j + 1] = sub_problems[i, j] + [all_x[i]]
+    for i, x in enumerate(xs):
+        for j, y in enumerate(ys):
+            if x == y:
+                sub_problems[i + 1, j + 1] = sub_problems[i, j] + [x]
             else:
                 sub_problems[i + 1, j + 1] = max(
                     sub_problems[i, j + 1],
                     sub_problems[i + 1, j],
                     key=len)
 
-    lcs = sub_problems[num_x, num_y]
+    lcs = sub_problems[len(xs), len(ys)]
 
-    return lcs, sequence_in_lcs(all_x, lcs), sequence_in_lcs(all_y, lcs)
+    return lcs, sequence_in_lcs(xs, lcs), sequence_in_lcs(ys, lcs)
 
-def sequence_in_lcs(x: Iterable, lcs: List) -> List[bool]:
+def sequence_in_lcs(xs: Iterable, lcs: List) -> List[bool]:
     """
     Determine which characters from a sequence are in another
+
+    >>> sequence_in_lcs("abcde", ["a","c"])
+    [True, False, True, False, False]
+    >>> sequence_in_lcs("a", ["a","c"])
+    [True]
     """
     x_in_lcs = []
     n = 0
-    for element in x:
-        if element == lcs[n]:
+    for x in xs:
+        if len(lcs) > n and x == lcs[n]:
             x_in_lcs.append(True)
             n += 1
         else:
