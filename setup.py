ddc29ac2dd47f6da5cc8b9952b798c633e40773b
Marco Caselli <casellimarco7@gmail.com>
2021-11-14 11:55:48 +0000

    Verify version and pass description

--- setup.py
+++ setup.py
@@ -1,9 +1,36 @@
+import os
+import sys
+
 from setuptools import setup
+from setuptools.command.install import install
+
+from get_version import get_version
+
+version = get_version()
+with open('README.md') as f:
+    long_description = f.read()
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != version:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, version
+            )
+            sys.exit(info)
 
 setup(
     install_requires=["deepdiff"],
-    version = "0.0.1",
+    version = version,
     author="Marco Caselli, Will Lohrmann",
     packages=["dast"],
-    name = "dast"
+    name = "dast",
+    long_description=long_description,
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )
