3e204139669da46e24e80cc809dc1c6df92929ec
Will Lohrmann <willia2501@gmail.com>
2021-11-20 12:29:37 +0100

    Move pretty diff into separate module

--- dast/__main__.py
+++ dast/__main__.py
@@ -17,16 +17,8 @@ from functools import partial
 
 from deepdiff import DeepDiff
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from dast.pretty_diff import print_diff
+
 
 
 _open_utf = partial(open, encoding="utf-8")
@@ -52,67 +44,9 @@ def main(then_path: str, now_path: str, verbose: bool = True):
 
     return now_ast, then_ast, diff
 
-def describe_node(node):
-    if isinstance(node, ast.Module):
-        return "module body"
-    if isinstance(node, ast.If):
-        return f"if statement"
-    if isinstance(node, ast.AugAssign):
-        return f"augmented assignment"
-    if isinstance(node, ast.Call):
-        return f"call to function {ast.unparse(node.func)}"
-    if isinstance(node, ast.keyword):
-        return "keyword"
-    if isinstance(node, ast.Name):
-        return "variable"
-    if isinstance(node, ast.For):
-        return "for loop"
-    if isinstance(node, ast.FunctionDef):
-        return f"function definition '{node.name}'"
-
-    return str(node.__class__)
-
-def describe_change(then, now, change_type, path):
-    msg = ""
-
-    is_added = True
-    parent = describe_node(eval(path.replace("root", "then").rpartition(".")[0]))
-    if change_type == "iterable_item_removed":
-        node = describe_node(eval(path.replace("root", "then")))
-        msg += f"{node} removed from {parent}"
-        is_added = False
-    elif change_type in ["type_changes", "values_changed"]:
-        msg += f"Item changed in {parent}"
-    elif change_type == "iterable_item_added":
-        node = describe_node(eval(path.replace("root", "now")))
-        msg += f"{node} added to {parent}"
-        is_added = True
-    else:
-        return change_type + " unknown", None
-
-    return msg, is_added
 
 
-def print_diff(diff, now_path, then, now):
-    print(f"diff --git a/{now_path} b/{now_path}")
-    print(f"--- a/{now_path}")
-    print(f"+++ b/{now_path}")
-    for change_type, changes in diff.items():
-        for path, change in changes.items():
-            description, is_added = describe_change(then, now, change_type, path)
-            print(f"{bcolors.OKCYAN}@@ -{0},{0} +{0},{0} @@{bcolors.ENDC} {description}")
-            if isinstance(change, ast.AST):
-                colour = bcolors.OKGREEN if is_added else bcolors.WARNING
-                print(colour + ast.unparse(change) + bcolors.ENDC)
-            else:
-                print(f"{bcolors.WARNING}-{unparse(change['old_value'])}{bcolors.ENDC}")
-                print(f"{bcolors.OKGREEN}+{unparse(change['new_value'])}{bcolors.ENDC}")
 
-def unparse(maybe_node):
-    if isinstance(maybe_node, ast.AST):
-        return ast.unparse(maybe_node)
-    else:
-        return str(maybe_node)
 
 if __name__ == "__main__":
     now, then, diff = main(sys.argv[1], sys.argv[2])
---
+++ dast/pretty_diff.py
@@ -0,0 +1,80 @@
+from enum import Enum
+
+import ast
+
+class Colors:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    CYAN = '\033[96m'
+    END = '\033[0m'
+
+def print_diff(diff, now_path, then, now):
+    then_lines = ast.unparse(then)
+    now_lines = ast.unparse(now)
+    print(f"diff --git a/{now_path} b/{now_path}")
+    print(f"--- a/{now_path}")
+    print(f"+++ b/{now_path}")
+    for change_type, changes in diff.items():
+        for path, change in changes.items():
+            description, is_added = describe_change(then, now, change_type, path)
+            print(f"{Colors.CYAN}@@ -{0},{0} +{0},{0} @@{Colors.END} {description}")
+            if isinstance(change, ast.AST):
+                haha(change, is_added)
+            else:
+                haha(change['old_value'], False)
+                haha(change['new_value'], True)
+
+
+def haha(change, is_added):
+    colour = Colors.GREEN if is_added else Colors.RED
+    symbol = "+" if is_added else "-"
+    change_text = symbol + ast.unparse(change).replace("\n", "\n" + symbol)
+    print(colour + change_text + Colors.END)
+
+
+def unparse(maybe_node):
+    if isinstance(maybe_node, ast.AST):
+        return ast.unparse(maybe_node)
+    else:
+        return str(maybe_node)
+
+def describe_node(node):
+    if isinstance(node, ast.Module):
+        return "module body"
+    if isinstance(node, ast.If):
+        return f"if statement"
+    if isinstance(node, ast.AugAssign):
+        return f"augmented assignment"
+    if isinstance(node, ast.Call):
+        return f"call to function {ast.unparse(node.func)}"
+    if isinstance(node, ast.keyword):
+        return "keyword"
+    if isinstance(node, ast.Name):
+        return "variable"
+    if isinstance(node, ast.For):
+        return "for loop"
+    if isinstance(node, ast.FunctionDef):
+        return f"function definition '{node.name}'"
+
+    return str(node.__class__)
+
+def describe_change(then, now, change_type, path):
+    msg = ""
+
+    is_added = True
+    parent = describe_node(eval(path.replace("root", "then").rpartition(".")[0]))
+    if change_type == "iterable_item_removed":
+        node = describe_node(eval(path.replace("root", "then")))
+        msg += f"{node} removed from {parent}"
+        is_added = False
+    elif change_type in ["type_changes", "values_changed"]:
+        node = describe_node(eval(path.replace("root", "then")))
+        msg += f"{node} changed in {parent}"
+    elif change_type == "iterable_item_added":
+        node = describe_node(eval(path.replace("root", "now")))
+        msg += f"{node} added to {parent}"
+        is_added = True
+    else:
+        return change_type + " unknown", None
+
+    return msg, is_added
