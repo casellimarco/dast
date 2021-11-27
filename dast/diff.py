2f181e583070583a1e89077814199c8ce04d0c4d
Marco Caselli <casellimarco7@gmail.com>
2021-11-27 12:52:06 +0000

    Get mapping at any depth

--- dast/diff.py
+++ dast/diff.py
@@ -1,8 +1,163 @@
+"""
+This script contains only modifications to deepdiff core classes.
+The edits are clearly highlighted through comments, anything else is just copy pasted from source
+"""
+
+
 from deepdiff.diff import (PASSES_COUNT, notpresent_indexed, SubscriptableIterableRelationship,
                            DeepDiff, OrderedSetPlus, notpresent, add_to_frozen_set)
 
+from deepdiff.deephash import (DeepHash, EMPTY_FROZENSET, dict_, logger, get_id, KEY_TO_VAL_STR, unprocessed)
+
+class Hash(DeepHash):
+    def _prep_iterable(self, obj, parent, parents_ids=EMPTY_FROZENSET):
+
+        counts = 1
+# --- begin diff wrt to deepdiff ---
+        result = set()
+# --- end diff wrt to deepdiff ---
+
+        for i, item in enumerate(obj):
+            new_parent = "{}[{}]".format(parent, i)
+            if self._skip_this(item, parent=new_parent):
+                continue
+
+            item_id = get_id(item)
+            if parents_ids and item_id in parents_ids:
+                continue
+
+            parents_ids_added = add_to_frozen_set(parents_ids, item_id)
+            hashed, count = self._hash(item, parent=new_parent, parents_ids=parents_ids_added)
+            # counting repetitions
+            counts += count
+# --- begin diff wrt to deepdiff ---
+            result.add((hashed, i))
+
+        if self.ignore_repetition:
+            result = list(set(map(lambda x: x[0], result)))
+        else:
+            result = [
+                f'{hashed}|{i}' for hashed, i in result
+            ]
+# --- end diff wrt to deepdiff ---
+
+        result = sorted(map(str, result))  # making sure the result items are string and sorted so join command works.
+        result = ','.join(result)
+        result = KEY_TO_VAL_STR.format(type(obj).__name__, result)
+
+        return result, counts
+
 class Diff(DeepDiff):
     mapping = {}
+
+    def _get_matching_pairs(self, level):
+        """
+        Given a level get matching pairs. This returns list of two tuples in the form:
+        [
+          (t1 index, t2 index), (t1 item, t2 item)
+        ]
+
+        This will compare using the passed in `iterable_compare_func` if available.
+        Default it to compare in order
+        """
+
+        if(self.iterable_compare_func is None):
+            # Match in order if there is no compare function provided
+            return self._compare_in_order(level)
+        try:
+            matches = []
+            y_matched = set()
+            y_index_matched = set()
+            for i, x in enumerate(level.t1):
+                x_found = False
+                for j, y in enumerate(level.t2):
+
+                    if(j in y_index_matched):
+                        # This ensures a one-to-one relationship of matches from t1 to t2.
+                        # If y this index in t2 has already been matched to another x
+                        # it cannot have another match, so just continue.
+                        continue
+
+                    if(self.iterable_compare_func(x, y, level)):
+# --- begin diff wrt to deepdiff ---
+                        deep_hash = Hash(y,
+# --- end diff wrt to deepdiff ---
+                                             hashes=self.hashes,
+                                             apply_hash=True,
+                                             **self.deephash_parameters,
+                                             )
+                        y_index_matched.add(j)
+                        y_matched.add(deep_hash[y])
+                        matches.append(((i, j), (x, y)))
+                        x_found = True
+                        break
+
+                if(not x_found):
+                    matches.append(((i, -1), (x, ListItemRemovedOrAdded)))
+            for j, y in enumerate(level.t2):
+
+# --- begin diff wrt to deepdiff ---
+                deep_hash = Hash(y,
+# --- end diff wrt to deepdiff ---
+                                     hashes=self.hashes,
+                                     apply_hash=True,
+                                     **self.deephash_parameters,
+                                     )
+                if(deep_hash[y] not in y_matched):
+                    matches.append(((-1, j), (ListItemRemovedOrAdded, y)))
+            return matches
+        except CannotCompare:
+            return self._compare_in_order(level)
+
+    def _create_hashtable(self, level, t):
+        """Create hashtable of {item_hash: (indexes, item)}"""
+        obj = getattr(level, t)
+
+        local_hashes = dict_()
+        for (i, item) in enumerate(obj):
+            try:
+                parent = "{}[{}]".format(level.path(), i)
+                # Note: in the DeepDiff we only calculate the hash of items when we have to.
+                # So self.hashes does not include hashes of all objects in t1 and t2.
+                # It only includes the ones needed when comparing iterables.
+                # The self.hashes dictionary gets shared between different runs of DeepHash
+                # So that any object that is already calculated to have a hash is not re-calculated.
+# --- begin diff wrt to deepdiff ---
+                deep_hash = Hash(item,
+# --- end diff wrt to deepdiff ---
+                                     hashes=self.hashes,
+                                     parent=parent,
+                                     apply_hash=True,
+                                     **self.deephash_parameters,
+                                     )
+                item_hash = deep_hash[item]
+            except Exception as e:  # pragma: no cover
+                logger.error("Can not produce a hash for %s."
+                             "Not counting this object.\n %s" %
+                             (level.path(), e))
+            else:
+                if item_hash is unprocessed:  # pragma: no cover
+                    logger.warning("Item %s was not processed while hashing "
+                                   "thus not counting this object." %
+                                   level.path())
+                else:
+                    self._add_hash(hashes=local_hashes, item_hash=item_hash, item=item, i=i)
+
+        # Also we hash the iterables themselves too so that we can later create cache keys from those hashes.
+        try:
+# --- begin diff wrt to deepdiff ---
+            Hash(
+# --- end diff wrt to deepdiff ---
+                obj,
+                hashes=self.hashes,
+                parent=level.path(),
+                apply_hash=True,
+                **self.deephash_parameters,
+            )
+        except Exception as e:  # pragma: no cover
+            logger.error("Can not produce a hash for iterable %s. %s" %
+                         (level.path(), e))
+        return local_hashes
 
     def _diff_iterable_with_deephash(self, level, parents_ids, _original_type=None):
         """Diff of hashable or unhashable iterables. Only used when ignoring the order."""
@@ -40,7 +195,7 @@ class Diff(DeepDiff):
         else:
             pairs = dict_()
 
-        # Added block
+# --- begin diff wrt to deepdiff ---
         mapping = []
         for maybe_t1_hash, maybe_t2_hash in pairs.items():
             if maybe_t1_hash in t1_hashes:
@@ -53,9 +208,9 @@ class Diff(DeepDiff):
             t2_index = t2_hashes.index(common_hash)
             if t1_index != t2_index:
                 mapping.append((t1_index, t2_index))
-
         if mapping:
             self.mapping[level] = mapping
+# --- end diff wrt to deepdiff ---
 
         def get_other_pair(hash_value, in_t1=True):
             """
